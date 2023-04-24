1151fd9c0220ec454f43f74d5fa6aeea80cbc56e
maruakinu <marloaquino080621@gmail.com>
2023-04-24 23:04:34 +0800

    Project Initiliazition

---
+++ src/main/java/com/demo/postgresqlspring/PostgresqlSpringApplication.java
@@ -0,0 +1,13 @@
+package com.demo.postgresqlspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresqlSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresqlSpringApplication.class, args);
+	}
+
+}
