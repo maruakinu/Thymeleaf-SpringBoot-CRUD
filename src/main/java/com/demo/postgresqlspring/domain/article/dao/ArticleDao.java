ca1ad51df2cd7d9ec6ed013ddc6321d1d666b32a
maruakinu <marloaquino080621@gmail.com>
2023-05-01 23:52:47 +0800

    Get article by Id is in progress

--- src/main/java/com/demo/postgresqlspring/domain/article/dao/ArticleDao.java
+++ src/main/java/com/demo/postgresqlspring/domain/article/dao/ArticleDao.java
@@ -5,6 +5,8 @@ import com.demo.postgresqlspring.domain.article.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public class ArticleDao {
 
@@ -14,6 +16,14 @@ public class ArticleDao {
     // save student
     public ArticleEntity saveArticle(ArticleEntity article) {
         return articleRepository.save(article);
+    }
+
+    // get student by id
+    public ArticleEntity getStudentById(int id) {
+        Optional<ArticleEntity> optional = articleRepository.findById(id);
+        if(optional.isEmpty()) {
+            return null;
+        } else return optional.get();
     }
 
 
--- src/main/java/com/demo/postgresqlspring/domain/article/repository/ArticleRepository.java
+++ src/main/java/com/demo/postgresqlspring/domain/article/repository/ArticleRepository.java
@@ -3,6 +3,6 @@ package com.demo.postgresqlspring.domain.article.repository;
 import com.demo.postgresqlspring.domain.article.entity.ArticleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {
+public interface ArticleRepository extends JpaRepository<ArticleEntity, Integer> {
 
 }
