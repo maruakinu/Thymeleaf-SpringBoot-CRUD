f40c6eb4240e4c29c9de5c981dda0f2d6892b2c8
Marlo Aquino <marlo.aquino@dysrupit.com.au>
2023-11-03 19:00:01 +0800

    Adding Thymeleaf with CRUD Features

--- src/main/java/com/demo/postgresqlspring/domain/article/controller/ArticleController.java
+++ src/main/java/com/demo/postgresqlspring/domain/article/controller/ArticleController.java
@@ -2,34 +2,74 @@ package com.demo.postgresqlspring.domain.article.controller;
 
 import com.demo.postgresqlspring.domain.article.dto.ArticleDto;
 import com.demo.postgresqlspring.domain.article.entity.ArticleEntity;
+import com.demo.postgresqlspring.domain.article.repository.ArticleRepository;
 import com.demo.postgresqlspring.domain.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api")
-@RequiredArgsConstructor
+//@RestController
+@Controller
 public class ArticleController {
 
     @Autowired
     ArticleService articleService;
 
-    @PostMapping("/post")
-    public ArticleDto<ArticleEntity> saveArticle(@RequestBody ArticleEntity article){
-        return articleService.saveArticle(article);
+    @Autowired
+    ArticleRepository articleRepository;
+
+//    @PostMapping("/post")
+//    public ArticleDto<ArticleEntity> saveArticle(@RequestBody ArticleEntity article){
+//        return articleService.saveArticle(article);
+//    }
+//    @PostMapping("/post")
+//    public ArticleDto<ArticleEntity> saveArticle(@RequestBody ArticleEntity article){
+//        return articleService.saveArticle(article);
+//    }
+
+    @PostMapping("/saveArticle")
+    public String saveEmployee(@ModelAttribute("article") ArticleEntity article) {
+        // save employee to database
+        articleService.saveArticle(article);
+        return "redirect:/";
     }
 
-    @GetMapping("/article/{id}")
-    public ArticleDto<ArticleEntity> getArticleById(@PathVariable int id){
-        return articleService.getArticleById(id);
+//    @GetMapping("/article/{id}")
+//    public ArticleDto<ArticleEntity> getArticleById(@PathVariable int id){
+//        return articleService.getArticleById(id);
+//    }
+
+    @GetMapping("/getArticleByID/{id}")
+    public String editTutorial(@PathVariable("id") Integer id, Model model) {
+
+        ArticleEntity article = articleRepository.findByID(id);
+
+            model.addAttribute("article", article);
+
+            return "update_article";
+
     }
 
-    @GetMapping("/all")
-    public ArticleDto<List<ArticleEntity>> getallArticles(){
-        return articleService.getAllArticles();
+//    @GetMapping("/all")
+//    public ArticleDto<List<ArticleEntity>> getallArticles(){
+//        return articleService.getAllArticles();
+//    }
+    @GetMapping("/")
+    public ModelAndView getAll(){
+        List<ArticleEntity> articles = articleRepository.findAll();
+        return new ModelAndView("index").addObject("articles", articles);
+    }
+
+    @GetMapping("/showNewArticleForm")
+    public String showNewEmployeeForm(Model model) {
+        ArticleEntity article = new ArticleEntity();
+        model.addAttribute("article", article);
+        return "add_article";
     }
 
     @PutMapping("/article/{id}")
@@ -37,11 +77,18 @@ public class ArticleController {
         return articleService.updateStudent(article, id);
     }
 
-    @DeleteMapping("/article/{id}")
-    public ArticleDto<String> deleteArticle(@PathVariable int id){
-        return articleService.deleteArticle(id);
-    }
+//    @DeleteMapping("/article/{id}")
+//    public ArticleDto<String> deleteArticle(@PathVariable int id){
+//        return articleService.deleteArticle(id);
+//    }
 
+    @GetMapping("/deleteArticle/{id}")
+    public String deleteEmployee(@PathVariable(value = "id") int id) {
+
+        // call delete employee method
+        articleService.deleteArticle(id);
+        return "redirect:/";
+    }
 
 
 
